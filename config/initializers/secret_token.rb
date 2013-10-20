a85ec64579fd238c1bfeade6ff52f85ec2361d9d
Blake Ruddock <ruddock.blake@gmail.com>
2013-10-20 12:22:42 -0400

    setup rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarketSentimentAnalysis::Application.config.secret_key_base = 'a62b03f4118d8fa97fb95339370df62d8ea2ed6e11ed5bbb6c1446321840a575b834338f2b26da1373375a72d626515f578b487e5cc364737898caae32862bf4'
