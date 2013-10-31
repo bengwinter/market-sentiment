a57824d220a9018a14163cb1b21c114b88fb5cbc
Blake Ruddock <ruddock.blake@gmail.com>
2013-10-30 22:18:37 -0400

    Removed comments from routes file and updated rails timezone to EST

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ module MarketSentimentAnalysis
   class Application < Rails::Application
 
     config.autoload_paths += Dir[File.join(Rails.root, "lib", "core_ext", "*.rb")].each {|l| require l }
-
+    config.time_zone = 'Eastern Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
