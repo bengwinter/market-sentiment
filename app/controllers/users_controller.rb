3b38e135e969c43b02d4c9a31b8b3e9216749c8a
Blake Ruddock <ruddock.blake@gmail.com>
2013-11-03 11:42:05 -0500

    Combined all user twilio methods into the users controller for simplicity, updated routes to correspond. Removed user update method because it was unused.  Testing new configuraiton on heroku.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,34 +7,48 @@ class UsersController < ApplicationController
   # process the signup form
 	def create
 	  	@user = User.new(user_params)
-	  	client = Twilio::REST::Client.new ENV['TWILIO_ID'], ENV['TWILIO_TOKEN']
 	  	if @user.save
-	  		client.account.messages.create(
-		        :from => '+16175443662',
-		        :to => @user.phone_number.to_s,
-		       :body => "Welcome to Sentimyzer! Please respond 'yes' to verify your phone number!"
-		      )
+		  	User.send_text('+16175443662', @user.phone_number.to_s, "Welcome to Sentimyzer! Please respond 'yes' to verify your phone number!")
 	  		flash[:notice] = "Thanks for signing up. Please respond to our text message to verify your phone number." 
-			cookies.permanent[:auth_token] = @user.auth_token
+				cookies.permanent[:auth_token] = @user.auth_token
 	  		redirect_to root_url
 	  	else
 	  		redirect_to root_url, :notice => "Registration failed. Please try again."
 	  	end
 	end
 
+	# verify user phone number
+	def verify_number
+		sender = params[:From].to_s
+		body = params[:Body].to_s.downcase
+		@user = User.find_by_phone_number(sender)
 
-	def update
-	  	user_id = current_user.id
-	  	@user = User.find(user_id)
-
-	    respond_to do |format|
-	      if @user.update(user_params)
-	        format.html { render action: 'show' , notice: 'Your profile was successfully updated.' }
-	      else
-	        format.html { render action: 'edit' }
-	      end
-	    end
+		if @user
+			if body == 'yes'
+				@user.update(verified: true)
+				User.send_text('+16175443662', sender, "Thank you! We've verified your number and added you to the Sentimyzer update list")
+			else 
+				User.send_text('+16175443662', sender, "Whoops. Please respond 'yes' to verify your account. We can't process other responses")
+			end
+		else
+			User.send_text('+16175443662', sender, "Sorry, we don't recognize your number!  Please sign up at http://sentimyzer.com")
+		end
 	end
+
+	# update a user profile 
+	# code may be unused --- check this. 
+	# def update
+	#   	user_id = current_user.id
+	#   	@user = User.find(user_id)
+
+	#     respond_to do |format|
+	#       if @user.update(user_params)
+	#         format.html { render action: 'show' , notice: 'Your profile was successfully updated.' }
+	#       else
+	#         format.html { render action: 'edit' }
+	#       end
+	#     end
+	# end
 
 	 
 	def destroy
@@ -44,29 +58,42 @@ class UsersController < ApplicationController
     	user.destroy
 
     	redirect_to root_url, notice: 'You were successfully unsubscribed from our alert list'
-  	end
+  end
 
+				User.send_text('+16175443662', sender, "Thank you! We've verified your number and added you to the Sentimyzer update list")
 
-  	def send_password
-  		client = Twilio::REST::Client.new ENV['TWILIO_ID'], ENV['TWILIO_TOKEN']
-  		if current_user
-	  		client.account.messages.create(
-	        :from => '+16175443963',
-	        :to => current_user.phone_number,
-	       :body => 'You requested to reset your password on Sentimyzer.com. Respond to this text with your desired new password.'
-	      )
-      		redirect_to root_url, notice: 'Respond to our text message with your desired new password.'
-  		elsif params[:phone_number]
-  			client.account.messages.create(
-	        :from => '+16175443963',
-	        :to => params[:phone_number],
-	       :body => 'You requested to reset your password on Sentimyzer.com. Respond to this text with your desired new password.'
-	      )
-      		redirect_to root_url, notice: 'Respond to our text message with your desired new password.'
-  		else
-  			redirect_to root_url, notice: 'We did not recognize that phone number.  Please try again or create a new account.'
-  		end
-  	end
+	# send password to user who forgets or wants to reset password
+	def send_password
+		if current_user
+			# for signed in users who request to change password
+			User.send_text('+16175443963', current_user.phone_number, "You requested to reset your password on Sentimyzer.com. Respond to this text with your desired new password.")
+    	redirect_to root_url, notice: 'Respond to our text message with your desired new password.'
+		elsif params[:phone_number]
+			# for users who forget their password and use the forget password form
+			User.send_text('+16175443963', params[:phone_number], "You requested to reset your password on Sentimyzer.com. Respond to this text with your desired new password.")
+    	redirect_to root_url, notice: 'Respond to our text message with your desired new password.'
+		else
+			redirect_to root_url, notice: 'We did not recognize that phone number.  Please try again or create a new account.'
+		end
+	end
+
+	# reset user password upon response
+	def reset_password
+		sender = params[:From].to_s
+		body = params[:Body].to_s		
+		@user = User.find_by_phone_number(sender)
+
+		if @user
+			@user.update(password: body)
+			if @user.save!
+				User.send_text('+16175443963', sender, "Great! We've saved your new password!")				
+			else 
+				User.send_text('+16175443963', sender, "We couldn't save your new password.  Please try again.")	
+			end
+		else 
+			User.send_text('+16175443963', sender, "Sorry, we don't recognize your number!  Please sign up at http://sentimyzer.com")	
+		end
+	end
   
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ MarketSentimentAnalysis::Application.routes.draw do
   get 'delete' => "users#destroy", :as => "delete_user"
   get 'new_password' => 'users#send_password', :as => 'new_password'
   post 'send_password' => 'users#send_password', :as => 'send_password'
-  get 'verify_number' => 'twilio#verify_number', :as => 'verify_number'
-  get 'reset_password' => 'twilio#reset_password', :as => 'reset_password'
+  get 'verify_number' => 'users#verify_number', :as => 'verify_number'
+  get 'reset_password' => 'users#reset_password', :as => 'reset_password'
 
   resources :sessions
 
