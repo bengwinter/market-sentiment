0703ef1c1f06f2533086eff672fe11cbc6577f14
Blake Ruddock <ruddock.blake@gmail.com>
2013-11-03 12:36:00 -0500

    DRYed up user model. Removed need for password confirmation on user creation, as there is no confirm password field in registration field

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,21 @@ require 'bcrypt'
 class User < ActiveRecord::Base
 	before_save :encrypt_password
 	before_create { generate_token(:auth_token) }
-
 	include BCrypt
 	attr_accessor :password
-
-	  validates_confirmation_of :password
-	  validates_presence_of :password, :on => :create
-	  validates_presence_of :phone_number
-	  validates_uniqueness_of :phone_number
-	  validates_format_of :phone_number, 
-                    :with => /1?\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})/, 
-                    :allow_blank => false, 
-                    :allow_nil => false
+  validates_presence_of :password, :on => :create
+  validates_presence_of :phone_number
+  validates_uniqueness_of :phone_number
+  validates_format_of :phone_number, 
+                  :with => /1?\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})/, 
+                  :allow_blank => false, 
+                  :allow_nil => false
 
 	def generate_token(column)
 		begin 
 			self[column] = SecureRandom.urlsafe_base64
 		end while User.exists?(column => self[column])
 	end
-
 
 	def encrypt_password
 		if password.present?
@@ -32,7 +28,6 @@ class User < ActiveRecord::Base
 		end
 	end	
 
-
 	def self.authenticate(phone_number, password)
 		user = self.find_by_phone_number(phone_number)
 		if user && user.password_digest == ::BCrypt::Engine.hash_secret(password, user.password_salt)
@@ -41,7 +36,6 @@ class User < ActiveRecord::Base
 			nil
 		end
 	end
-
 
 	def self.send_text(sender, recipient, body)
 		$twilio_client.account.messages.create(
